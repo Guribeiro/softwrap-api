713dbb6bd33750014d5a0fc69e0ca31965b63806
Gustavo Ribeiro <gugahribeiro@hotmail.com>
2021-04-24 22:24:57 -0300

    chore: add celebrate routes validation

--- src/modules/admin/infra/http/routes/admin.routes.ts
+++ src/modules/admin/infra/http/routes/admin.routes.ts
@@ -1,10 +1,17 @@
 import { Router } from 'express';
 import AdminsController from '../controllers/AdminsController';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 const adminRouter = Router()
 
 const adminsController = new AdminsController();
 
-adminRouter.post('/', adminsController.create);
+adminRouter.post('/',  celebrate({
+  [Segments.BODY]: {
+    name: Joi.string().required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+  },
+}), adminsController.create);
 
 export default adminRouter;
--- src/modules/customer/infra/http/routes/customer.routes.ts
+++ src/modules/customer/infra/http/routes/customer.routes.ts
@@ -1,21 +1,41 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 import CustomersController from '../controllers/CustomersController';
+
 const customerRouter = Router();
+
+import ensureAuthenticated from '@modules/admin/infra/http/middlewares/ensureAuthenticated';
 
 const customersController = new CustomersController();
 
-customerRouter.post('/', customersController.create);
+customerRouter.post('/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      age: Joi.number().required(),
+      marital_status: Joi.string().required(),
+      cpf: Joi.string().required(),
+      city: Joi.string().required(),
+      state: Joi.string().required(),
+    }
+  }),
+  ensureAuthenticated, customersController.create);
 
+customerRouter.get('/', ensureAuthenticated, customersController.index);
 
-// customerRouter.get('/', async (request, response) => {
-//   try {
-//     const customersRepository = getCustomRepository(CustomersRepository);
-//     const customers = await customersRepository.index();
+customerRouter.get('/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required()
+    }
+  }),
+  ensureAuthenticated, customersController.show);
 
-//     return response.json(customers);
-//   } catch (error) {
-//     return response.json({ error: error.message })
-//   }
-// })
+customerRouter.put('/:customer_id',
+  celebrate({
+    [Segments.PARAMS]: {
+      customer_id:Joi.string().uuid().required()
+    }
+  }), ensureAuthenticated, customersController.update);
 
 export default customerRouter;
