65d6de1dd3bb6b7673199420c2f05e13ea271f27
Gustavo Ribeiro <gugahribeiro@hotmail.com>
2021-04-24 22:34:24 -0300

    chore: add providers

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,18 +1,29 @@
 import { container } from 'tsyringe';
 
+import '@modules/admin/providers';
+import '@shared/container/providers';
+
 import IAdminsRepository from '@modules/admin/infra/repositories/IAdminsRepository';
 import AdminsRepository from '@modules/admin/infra/typeorm/repositories/AdminsRepository';
+
+import IAdminsTokenRepository from '@modules/admin/infra/repositories/IAdminsTokenRepository';
+import AdminTokensRepository from '@modules/admin/infra/typeorm/repositories/AdminTokensRepository';
 
 import ICustomersRepository from '@modules/customer/infra/repositories/ICustomersRepository';
 import CustomersRepository from '@modules/customer/infra/typeorm/repositories/CustomersRepository';
 
-container.registerSingleton<ICustomersRepository>(
-  'CustomersRepository',
-  CustomersRepository
-)
-
 container.registerSingleton<IAdminsRepository>(
   'AdminsRepository',
   AdminsRepository
+)
+
+container.registerSingleton<IAdminsTokenRepository>(
+  'AdminTokensRepository',
+  AdminTokensRepository
+)
+
+container.registerSingleton<ICustomersRepository>(
+  'CustomersRepository',
+  CustomersRepository
 )
 
---
+++ src/shared/container/providers/index.ts
@@ -0,0 +1,2 @@
+import './EmailTemplateProvider';
+import './MailProvider';
