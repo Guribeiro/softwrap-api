d7b15f8f5d487142a5180db4216da789327a6e5d
Gustavo Ribeiro <gugahribeiro@hotmail.com>
2021-04-24 22:28:14 -0300

    chore: create customers entities

--- src/modules/customer/infra/typeorm/entities/Customer.ts
+++ src/modules/customer/infra/typeorm/entities/Customer.ts
@@ -1,4 +1,4 @@
-import {Entity, PrimaryGeneratedColumn, Column} from 'typeorm';
+import {Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn} from 'typeorm';
 
 @Entity('customers')
 class Customer {
@@ -23,6 +23,12 @@ class Customer {
 
   @Column()
   state: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
 }
 
 
