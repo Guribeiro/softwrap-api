43f236b3666b461d8788e1a4286948d9403fe600
Gustavo Ribeiro <gugahribeiro@hotmail.com>
2021-04-24 22:34:42 -0300

    chore: add celebrate errors

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,6 +1,7 @@
 import "reflect-metadata";
 import 'dotenv/config';
 import express, {Request, Response, NextFunction} from 'express';
+import {errors} from 'celebrate';
 import routes from './routes';
 import AppError from '@shared/errors/AppError';
 import '@shared/container';
@@ -12,6 +13,8 @@ const app = express();
 app.use(express.json());
 
 app.use(routes);
+
+app.use(errors());
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   if (err instanceof AppError) {
