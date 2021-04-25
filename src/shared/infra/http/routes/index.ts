fab32b6ae26ed8ef72c136ccf0caf4e7ed92a081
Gustavo Ribeiro <gugahribeiro@hotmail.com>
2021-04-24 22:35:00 -0300

    chore: add password router

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -5,10 +5,12 @@ import ensureAuthenticated from '@modules/admin/infra/http/middlewares/ensureAut
 import customerRouter from '@modules/customer/infra/http/routes/customer.routes';
 import sessionRouter from '@modules/admin/infra/http/routes/sessions.routes';
 import adminRouter from '@modules/admin/infra/http/routes/admin.routes';
+import passwordRouter from '@modules/admin/infra/http/routes/password.routes';
 
 const routes = Router();
 
 routes.use('/customers', ensureAuthenticated, customerRouter);
+routes.use('/password', passwordRouter);
 routes.use('/sessions', sessionRouter);
 routes.use('/', adminRouter);
 
